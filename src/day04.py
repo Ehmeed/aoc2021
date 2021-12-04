56f5d2f286fdd54a270f94687d6d4a5165bfc2e8
Milan Hruban <hruban.milan@gmail.com>
2021-12-04 07:46:15 +0100

    day 4

--- src/day04.py
+++ src/day04.py
@@ -4,12 +4,76 @@ from typing import Generator
 from util.util import read_lines
 
 filename = Path(__file__).stem
+# filename = 'test_day04'
 
 
-def day4(in_data: Generator[str, None, None]) -> int:
-    return 0
+def giant_squid(in_data: Generator[str, None, None], last_wins=False) -> int:
+    rows = 5
+    with open('/home/ehmeed/repos/aoc2021/input/' + filename) as file:
+        lines = file.readlines()
+    lines = [line.rstrip() for line in lines]
+    numbers = list(map(int, lines[0].split(",")))
 
+    boards = []
+    n_boards = 1 + int((len(lines) - 2) / (rows + 1))
+    for n in range(n_boards):
+        boards.append(_load_board(lines[2 + (rows + 1) * n:]))
+
+    n_winners = 0
+    drawn = numbers[:rows]
+    for number in numbers[rows:]:
+        drawn.append(number)
+        closed_boards = []
+        for board_id, board in enumerate(boards):
+            for row in board:
+                if _is_winning(row, drawn):
+                    board_score = score(board, drawn)
+                    if not last_wins or n_winners == n_boards - 1:
+                        return board_score
+                    closed_boards.append(board_id)
+            for n_col in range(len(board[0])):
+                col = []
+                for row in board:
+                    col.append(row[n_col])
+                if _is_winning(col, drawn):
+                    board_score = score(board, drawn)
+                    if not last_wins or n_winners == n_boards - 1:
+                        return board_score
+                    closed_boards.append(board_id)
+
+        closed_boards = set(closed_boards)
+        n_winners += len(closed_boards)
+        for index in sorted(list(closed_boards), reverse=True):
+            del boards[index]
+    return -1
+
+
+def score(board, drawn):
+    us = 0
+    for row in board:
+        for number in row:
+            if number not in drawn:
+                us += number
+
+    return us * drawn[-1]
+
+
+def _is_winning(numbers, drawn):
+    for number in numbers:
+        if number not in drawn:
+            return False
+    return True
+
+
+def _load_board(in_data) -> list[list[int]]:
+    board = []
+    for line in in_data:
+        if not line:
+            break
+        board.append(list(map(int, [it for it in line.split(" ") if it])))
+    return board
 
 
 if __name__ == "__main__":
-    print(day4(read_lines(filename)))
+    print(giant_squid(read_lines(filename)))
+    print(giant_squid(read_lines(filename), last_wins=True))
--- src/test/test_day04.py
+++ src/test/test_day04.py
@@ -2,7 +2,7 @@ import unittest
 from pathlib import Path
 
 from util.util import read_lines
-from day04 import day4
+from day04 import giant_squid
 
 filename = Path(__file__).stem
 
@@ -11,8 +11,13 @@ class TestDay4(unittest.TestCase):
 
     def test_part_one(self):
         in_data = read_lines(filename)
-        final_position = day4(in_data)
-        self.assertEqual(final_position, -1)
+        result = giant_squid(in_data)
+        self.assertEqual(result, 4512)
+
+    def test_part_two(self):
+        in_data = read_lines(filename)
+        result = giant_squid(in_data, last_wins=True)
+        self.assertEqual(result, 1924)
 
 if __name__ == '__main__':
     unittest.main()
